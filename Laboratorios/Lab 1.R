516ba2154970320cf02f441d218c0830d0da6888
fernandapmtz <166416821+fernandapmtz@users.noreply.github.com>
2024-05-30 11:55:58 -0600

    Add files via upload

---
+++ Laboratorios/Lab 1.R
@@ -0,0 +1,87 @@
+# Fernanda Paola Martinez Mendez
+# 2027016
+# 25/04/2024
+
+
+# Gastos totales
+300 + 240 + 1527 + 400 + 1500 + 1833
+celular <- 300
+celular
+transporte <- 240
+transporte
+comestibles <- 1527
+comestibles
+gimnasio <- 400
+gimnasio
+alquiler <- 1500
+alquiler
+otros <- 1833
+otros
+gastos <- 5800 # objeto total con la suma de datos
+gastos
+# Gastos durante semestre escolar
+5800 + 5800 + 5800 + 5800 + 5800
+# Gastos durante un año
+29000 + 29000
+gastos <- c(celular, transporte, comestibles, gimnasio, alquiler, otros)
+barplot(gastos)
+gastos_ordenados <- sort(gastos, decreasing = TRUE)
+barplot(gastos_ordenados)
+# Parte II Variables
+# Problema 1
+# Identifique el tipo de variable (cualitativa o cuantitativa) para la lista de preguntas de una encuesta
+# aplicada a estudiantes universitarios en una clase de estadistica:
+# Fecha de nacimiento (p. Ej., 21/10/1995) : cuantitativa
+# Nombre del estudiante: cualitativa
+# Edad: cuantitativa
+# Direccion de casa: cualitativa
+# Numero de telefono: cuantitativa
+# Area principal de estudio: cualitativa
+# Grado de año universitario: cualitativa
+# Puntaje de prueba a mitad de periodo: cuantitativa
+# Calificacion general: A, B, C, D. cualitativa
+# Tiempo para completar la prueba final de MCF 202: cuantitativa
+# Numero de hermanos: cuantitativa
+# Problema 2: elija un objeto y obtega una lista de 14 variables, 7 cuantitativas y 7 cualitativas
+# Cuantitativas
+# Peso
+# Altura
+# Edad
+# Periodo de ovulacion
+# Periodo de reproduccion
+# Numero de dientes
+# Peridoo de lactacion
+# Cualitativas
+# Clase
+# Tipo de sangre
+# Familia
+# Tipo de reproduccion
+# Color
+# Olor
+# Tipo de pelaje
+# Problema 3: considere una variable de investigacion con valores numericos que describen
+# fromas electronicas de expresar opiniones de personas: 1= Twitter, 2= Correo electronico ,3=
+# mensaje de texto, 4 = facebook, 5= blog ¿ Esta es una variable cualitativa o cuantitativa?
+# estas son variables cuantitativas ya que en todas se pueden obtener datos de opiniones, los que
+# estan a favor y los que estan en contra el numero total de usuarios entre otras cosas.
+# Problema 4:
+# Para cada pregunta de ingestigacion, (1) identifique a los individuos de interes
+# el grupo o los grupos que estan estudiando, (2) identifique las variable (s) ( la
+# caracteristicas sobre la que recopilariamos datos), (3) deterimne si cada variable
+# es categorigo cuantitativa  o cualitativa. Explique
+# ¿ Cual es la cantidad promedio de horas que los estudiantes de universidades publicas trabajan cada semana?
+#ht <- c(10, 14, 12, 18, 23, 15, 6, 9, 14, 24)
+# mean(ht)
+# ¿Que proporcion de todos los estudiantes universitarios de Mexico estan inscritos en una
+# universidad publica?
+#EdU <- (230000)
+#EdUPrivadas <-(170000)
+#EdU - EdUPrivadas
+# En las universidades publicas, ¿las estudiantes femeninas tienen un promedio de CENEVAL
+# mas alto que los estudinates varones? si
+# ¿Es mas probable que los atletas universitarios reciban asesoramientos academico
+# que los atletas no universitarios? no
+# Si reunieramos datos para responder a las preguntas de investigacion anterior, ¿que datos
+# podrian analizarse medinate un histograma? ¿Como lo sabes?
+# si, porque mediante el histograma se puede observar la cantidad de variables que estas tomando
+# para responder dichas preguntas.
